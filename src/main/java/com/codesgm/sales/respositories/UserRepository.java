ea9a8c970be933fa59cace19b73c3dfed95966fc
Guilherme <guilhermetts0@gmail.com>
2025-01-25 11:43:34 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/codesgm/sales/respositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.codesgm.sales.respositories;
+
+import com.codesgm.sales.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
