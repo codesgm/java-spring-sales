05ab2f8446b08ba3f76054dce82c41c80469f6c3
Guilherme <guilhermetts0@gmail.com>
2025-02-04 22:42:02 -0300

    add methods getTotal and getSubTotal

--- src/main/java/com/codesgm/sales/entities/Order.java
+++ src/main/java/com/codesgm/sales/entities/Order.java
@@ -94,6 +94,14 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public double getTotal(){
+        double sum = 0;
+        for (OrderItem x : items){
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
